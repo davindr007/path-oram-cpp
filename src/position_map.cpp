aa70ea628f849b2aab0f87f2bfe17c1b642519a9
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-06-20 13:56:33 +0530

    Create position_map.cpp

---
+++ src/position_map.cpp
@@ -0,0 +1,12 @@
+#include "position_map.hpp"
+
+PositionMap::PositionMap(int num_leaves) : max_leaf(num_leaves - 1) {}
+
+int PositionMap::getLeaf(int block_id) {
+    return pos_map[block_id];
+}
+
+void PositionMap::assignNewLeaf(int block_id) {
+    int random_leaf = rand() % (max_leaf + 1);
+    pos_map[block_id] = random_leaf;
+}
