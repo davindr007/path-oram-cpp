2c442ee7e8dd69ccf4f7a1df0aaaeba9c1480a0b
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-06-22 14:41:20 +0530

    Create stash.hpp

---
+++ include/stash.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "block.hpp"
+#include <vector>
+#include <optional>
+
+class Stash {
+private:
+    std::vector<Block> buffer;
+
+public:
+    void addBlock(const Block& block);
+    std::optional<Block> getBlock(int id);
+    void removeBlock(int id);
+    std::vector<Block> getAllBlocks() const;
+    void print() const;
+};
