37c2b7ac9deb5edd066bfb0176e52854a78dcb59
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-06-22 14:43:19 +0530

    Create accessor.hpp

---
+++ include/accessor.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "oram_tree.hpp"
+#include "position_map.hpp"
+#include "stash.hpp"
+#include <string>
+
+enum AccessType { READ, WRITE };
+
+class Accessor {
+private:
+    ORAMTree& tree;
+    PositionMap& pos_map;
+    Stash stash;
+
+public:
+    Accessor(ORAMTree& t, PositionMap& p);
+    std::string access(int block_id, AccessType type, std::string new_data = "");
+};
