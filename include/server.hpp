9c1616df6957e8b9846594ec01178d31a0fd90b1
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:44:40 +0530

    Update server.hpp

--- include/server.hpp
+++ include/server.hpp
@@ -1,31 +1,31 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 #include "oram_tree.hpp"
+#include <sstream>
 #include <iostream>
+
 class ORAMServer {
     ORAMTree oram;
 public:
     ORAMServer(int levels) : oram(levels) {}
-    void run() {
-        std::string cmd;
-        while (std::getline(std::cin, cmd)) {
-            std::istringstream iss(cmd);
-            std::string op;
-            int id;
-            iss >> op >> id;
-            
-            if (op == "READ") {
-                auto data = oram.read(id);
-                for (int x : data) std::cout << x << " ";
-                std::cout << "\n";
-            }
-            else if (op == "WRITE") {
-                std::vector<int> data;
-                int x;
-                while (iss >> x) data.push_back(x);
-                oram.write(id, data);
-                std::cout << "OK\n";
-            }
+    
+    void process_command(const std::string& cmd) {
+        std::istringstream iss(cmd);
+        std::string op;
+        int id;
+        iss >> op >> id;
+        
+        if (op == "READ") {
+            auto data = oram.read(id);
+            for (int x : data) std::cout << x << " ";
+            std::cout << std::endl;
+        }
+        else if (op == "WRITE") {
+            std::vector<int> data;
+            int val;
+            while (iss >> val) data.push_back(val);
+            oram.write(id, data);
+            std::cout << "OK" << std::endl;
         }
     }
 };
