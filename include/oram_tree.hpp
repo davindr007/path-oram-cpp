e730e6c1b97355398fa17777fefc5873cc2a98ab
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 14:56:42 +0530

    Update oram_tree.hpp

--- include/oram_tree.hpp
+++ include/oram_tree.hpp
@@ -1,17 +1,21 @@
-#pragma once
 #include "bucket.hpp"
+#include "block.hpp"
 #include <vector>
-#include <cmath>
 
 class ORAMTree {
-public:
-    std::vector<Bucket> tree;  // Tree stored as flat array of buckets
-    int height;                // Height of the tree (excluding root level)
-    int Z;                     // Blocks per bucket
+private:
+    int height;
+    int Z;
+    std::vector<Bucket> tree;
 
+    int getNodeIndex(int level, int index) const;
+    std::vector<int> pathToRoot(int leaf) const;
+
+public:
     ORAMTree(int h, int z);
-    int getLeafNodeCount() const;
-    int getNodeIndex(int level, int offset) const;
-    std::vector<int> getPathIndexes(int leaf) const;
-    void printPath(int leaf) const;
+
+    std::vector<Block> readPath(int leaf) const;
+    void writePath(int leaf, const std::vector<Block>& path);
+    bool insertBlockAtLeaf(int leaf, const Block& block);
 };
+
