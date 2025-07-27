8eeb52867d8c5e6d623fea879084039b654765f1
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 15:05:10 +0530

    Update position_map.cpp

--- src/position_map.cpp
+++ src/position_map.cpp
@@ -1,17 +1,24 @@
 #include "position_map.hpp"
+#include <cstdlib>  // For rand()
 
-PositionMap::PositionMap(int num_leaves) : max_leaf(num_leaves - 1) {}
-
-int PositionMap::getLeaf(int block_id) {
-    if (pos_map.find(block_id) != pos_map.end()) {
-    return pos_map[block_id];
-    }
-    else {
-    return -1; // Or handle error
+PositionMap::PositionMap(int num_blocks, int num_leaves) 
+    : num_blocks(num_blocks), num_leaves(num_leaves) {
+    map.resize(num_blocks);
+    for (int i = 0; i < num_blocks; ++i) {
+        map[i] = rand() % num_leaves;
     }
 }
 
-void PositionMap::assignNewLeaf(int block_id) {
-    int random_leaf = rand() % (max_leaf + 1);
-    pos_map[block_id] = random_leaf;
+int PositionMap::get_position(int block_id) const {
+    if (block_id < 0 || block_id >= num_blocks) {
+        throw std::out_of_range("Invalid block ID in get_position");
+    }
+    return map[block_id];
+}
+
+void PositionMap::set_position(int block_id, int leaf) {
+    if (block_id < 0 || block_id >= num_blocks) {
+        throw std::out_of_range("Invalid block ID in set_position");
+    }
+    map[block_id] = leaf;
 }
