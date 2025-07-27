2efb96108f08e7d141fd6c03fd8c02352d9150bd
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:39:18 +0530

    Update block.hpp

--- include/block.hpp
+++ include/block.hpp
@@ -1,20 +1,9 @@
 #ifndef BLOCK_HPP
 #define BLOCK_HPP
-
 #include <vector>
-#include <cstdint>
-#include <string>
-
-class Block {
-public:
-    int id;  // -1 for dummy blocks
-    std::vector<uint8_t> data;
-    int leaf_id;
-    
-    Block(int id = -1, const std::vector<uint8_t>& data = {}, int leaf_id = 0);
-    bool is_dummy() const;
-    size_t size() const;
-    std::string to_string() const;  // Debug method
+struct Block {
+    int id = -1; // -1 = dummy
+    std::vector<int> data;
+    int leaf = 0;
 };
-
 #endif
