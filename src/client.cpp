a7da55b7f434ede031dd81606d167ee27a46419c
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:41:13 +0530

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -1,38 +1,11 @@
+#include "accessor.hpp"
 #include <iostream>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string>
-#include <cstring>
-
-#define PORT 8080
-
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    std::cout << "Connected to server.\n";
-
-    // test write
-    std::string write_msg = "WRITE 42 HelloWorld";
-    send(sock, write_msg.c_str(), write_msg.size(), 0);
-    read(sock, buffer, 1024);
-    std::cout << "Write response: " << buffer << std::endl;
-
-    // test read
-    std::string read_msg = "READ 42";
-    send(sock, read_msg.c_str(), read_msg.size(), 0);
-    memset(buffer, 0, sizeof(buffer));
-    read(sock, buffer, 1024);
-    std::cout << "Read response: " << buffer << std::endl;
-
-    close(sock);
-    return 0;
+    ORAMAccessor oram(4); // 4-level tree
+    oram.write(1, {1,2,3});
+    oram.write(2, {4,5,6});
+    auto data = oram.read(1);
+    std::cout << "Read:";
+    for (int x : data) std::cout << " " << x;
+    std::cout << "\n";
 }
