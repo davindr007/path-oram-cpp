1caed024ea995a4a3a6364705a8bb7d3ff713db3
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:41:47 +0530

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,32 +1,5 @@
-#ifndef SERVER_HPP
-#define SERVER_HPP
-#include "oram_tree.hpp"
-#include <iostream>
-class ORAMServer {
-    ORAMTree oram;
-public:
-    ORAMServer(int levels) : oram(levels) {}
-    void run() {
-        std::string cmd;
-        while (std::getline(std::cin, cmd)) {
-            std::istringstream iss(cmd);
-            std::string op;
-            int id;
-            iss >> op >> id;
-            
-            if (op == "READ") {
-                auto data = oram.read(id);
-                for (int x : data) std::cout << x << " ";
-                std::cout << "\n";
-            }
-            else if (op == "WRITE") {
-                std::vector<int> data;
-                int x;
-                while (iss >> x) data.push_back(x);
-                oram.write(id, data);
-                std::cout << "OK\n";
-            }
-        }
-    }
-};
-#endif
+#include "server.hpp"
+int main() {
+    ORAMServer server(4); // 4-level tree
+    server.run();
+}
