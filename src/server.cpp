f3b19b2676f653a0cc3fc98f859e2ae9f02b442c
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:44:09 +0530

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,18 @@
 #include "server.hpp"
-int main() {
-    ORAMServer server(4); // 4-level tree
-    server.run();
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <tree_levels>" << std::endl;
+        return 1;
+    }
+    
+    ORAMServer server(std::stoi(argv[1]));
+    
+    std::string cmd;
+    while (std::getline(std::cin, cmd)) {
+        server.process_command(cmd);
+    }
+    
+    return 0;
 }
