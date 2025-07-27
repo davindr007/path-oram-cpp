c3a4d40fbfd8329a0640f7752c21d9b416f246bd
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:40:32 +0530

    Update oram_tree.hpp

--- include/oram_tree.hpp
+++ include/oram_tree.hpp
@@ -1,27 +1,64 @@
 #ifndef ORAM_TREE_HPP
 #define ORAM_TREE_HPP
-
 #include "bucket.hpp"
-#include "position_map.hpp"
 #include "stash.hpp"
+#include "position_map.hpp"
 #include <vector>
-#include <string>
-
 class ORAMTree {
-private:
-    int height;
-    std::vector<Bucket> tree;
-    PositionMap posMap;
+    int levels;
+    std::vector<std::vector<Bucket>> tree;
     Stash stash;
-
-    int getLeafIndex(int leaf) const;
-    std::vector<int> getPathIndices(int leaf) const;
-
+    PositionMap pmap;
+    
+    std::vector<int> get_path(int leaf) const {
+        std::vector<int> path;
+        for (int l = 0; l < levels; l++)
+            path.push_back((leaf >> (levels - l - 1)));
+        return path;
+    }
+    
+    void evict_path(int leaf) {
+        auto path = get_path(leaf);
+        for (int lvl = 0; lvl < levels; lvl++) {
+            Bucket& bucket = tree[lvl][path[lvl]];
+            for (auto it = stash.begin(); it != stash.end(); ) {
+                Block& b = it->second;
+                auto b_path = get_path(b.leaf);
+                bool in_subtree = true;
+                for (int i = 0; i <= lvl; i++)
+                    if (b_path[i] != path[i]) { in_subtree = false; break; }
+                
+                if (in_subtree && bucket.add(b))
+                    it = stash.remove(b.id);
+                else
+                    ++it;
+            }
+        }
+    }
+    
 public:
-    ORAMTree(int height);
-
-    void access(const std::string& id, const std::string& data, const std::string& op);
-    void printPath(int leaf) const;
+    ORAMTree(int levels) : levels(levels) {
+        tree.resize(levels);
+        for (int l = 0; l < levels; l++)
+            tree[l].resize(1 << l);
+    }
+    
+    std::vector<int> read(int id) {
+        int leaf = pmap.get_leaf(id);
+        for (int lvl = 0; lvl < levels; lvl++)
+            for (auto& b : tree[lvl][get_path(leaf)[lvl]].remove_all())
+                stash.add(b);
+        
+        pmap.update(id, leaf);
+        evict_path(leaf);
+        return stash.get(id).data;
+    }
+    
+    void write(int id, const std::vector<int>& data) {
+        int leaf = pmap.get_leaf(id);
+        stash.add({id, data, leaf});
+        pmap.update(id, leaf);
+        evict_path(leaf);
+    }
 };
-
 #endif
