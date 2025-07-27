4fdd9188c3f0cfccd088ab26a279fec5d1438444
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-27 16:39:36 +0530

    Update bucket.hpp

--- include/bucket.hpp
+++ include/bucket.hpp
@@ -1,31 +1,20 @@
 #ifndef BUCKET_HPP
 #define BUCKET_HPP
-
 #include "block.hpp"
 #include <vector>
-
 class Bucket {
-public:
-    static const int Z = 4;  // Max number of blocks per bucket
     std::vector<Block> blocks;
-
-    Bucket() {
-        blocks.resize(Z, Block());  // Fill with dummy blocks
+    const int Z = 4; // Fixed bucket size
+public:
+    bool add(const Block& b) {
+        if (blocks.size() >= Z) return false;
+        blocks.push_back(b);
+        return true;
     }
-
-    bool insert(const Block& blk) {
-        for (auto& b : blocks) {
-            if (b.isDummy()) {
-                b = blk;
-                return true;
-            }
-        }
-        return false; // No space
-    }
-
-    std::vector<Block> getAllBlocks() const {
-        return blocks;
+    std::vector<Block> remove_all() {
+        auto temp = blocks;
+        blocks.clear();
+        return temp;
     }
 };
-
 #endif
