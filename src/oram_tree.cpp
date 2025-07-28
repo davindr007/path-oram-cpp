07cf8bb79ee301ab8869576689ed08b4fd03bab5
davindr007 <ds22csb0a40@student.nitw.ac.in>
2025-07-28 11:19:23 +0530

    Create oram_tree.cpp

---
+++ src/oram_tree.cpp
@@ -0,0 +1,39 @@
+#include "oram_tree.hpp"
+#include <iostream>
+#include <algorithm>
+
+ORAMTree::ORAMTree(int h, int z) : height(h), Z(z) {
+    int total_nodes = std::pow(2, height + 1) - 1;
+    tree = std::vector<Bucket>(total_nodes, Bucket(Z));
+}
+
+int ORAMTree::getLeafNodeCount() const {
+    return std::pow(2, height);
+}
+
+int ORAMTree::getNodeIndex(int level, int offset) const {
+    return std::pow(2, level) - 1 + offset;
+}
+
+std::vector<int> ORAMTree::getPathIndexes(int leaf) const {
+    std::vector<int> path;
+    int index = leaf + (1 << height) - 1;
+    std::cout << "[DEBUG] getPathIndexes: leaf=" << leaf << ", start index=" << index << "\n";
+
+    while (index > 0) {
+        path.push_back(index);
+        index = (index - 1) / 2;
+    }
+    path.push_back(0); // Add the root node
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+
+
+void ORAMTree::printPath(int leaf) const {
+    auto path = getPathIndexes(leaf);
+    for (int i : path) {
+        std::cout << "Bucket " << i << ": ";
+        tree[i].print();
+    }
+}
